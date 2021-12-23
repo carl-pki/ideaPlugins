937b233dc3897f22343c6a1d9145ca8041527bc1
GitHub Action <action@github.com>
2021-12-23 09:57:27 +0000

    Template cleanup

--- src/main/kotlin/com/github/carlpki/ideaplugins/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/carlpki/ideaplugins/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.carlpki.ideaplugins.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.carlpki.ideaplugins.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/carlpki/ideaplugins/services/MyProjectService.kt
+++ src/main/kotlin/com/github/carlpki/ideaplugins/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.carlpki.ideaplugins.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.carlpki.ideaplugins.MyBundle
 
 class MyProjectService(project: Project) {
 
